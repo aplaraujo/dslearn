85578a6656a638b767172bc5d0daea9a656bc5a1
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-20 16:11:12 -0300

    Course entity

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,4 +9,8 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Curso de HTML', 'https://upload.wikimedia.org/wikipedia/commons/d/de/HTML5_oval_logo.png', 'https://freesvg.org/img/1643312539html5.png');
