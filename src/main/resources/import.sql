cf64e0b33b4b343caff1d6e35d20c5a6eb6e8b96
Ana Paula Lopes Araujo <aplaraujo@gmail.com>
2025-10-20 21:35:59 -0300

    Enrollment, EnrollmentPK entities

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -15,9 +15,16 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
 
 INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Curso de HTML', 'https://upload.wikimedia.org/wikipedia/commons/d/de/HTML5_oval_logo.png', 'https://freesvg.org/img/1643312539html5.png');
 
-INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP '2020-11-20T03:00:00', TIMESTAMP '2021-11-20T03:00:00', 1);
-INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP '2021-12-20T03:00:00', TIMESTAMP '2022-11-20T03:00:00', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('1.0', TIMESTAMP '2020-11-20T03:00:00Z', TIMESTAMP '2021-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('2.0', TIMESTAMP '2021-12-20T03:00:00Z', TIMESTAMP '2022-11-20T03:00:00Z', 1);
 
 INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Trilha de HTML', 'Trilha principal do curso de HTML', 1, 'https://upload.wikimedia.org/wikipedia/commons/d/de/HTML5_oval_logo.png', 1, 1);
 INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Forum', 'Forum de discussão do curso', 2, 'https://upload.wikimedia.org/wikipedia/commons/d/de/HTML5_oval_logo.png', 2, 1);
 INSERT INTO tb_resource (title, description, position, img_uri, type, offer_id) VALUES ('Lives', 'Transmissões ao vivo do curso', 3, 'https://upload.wikimedia.org/wikipedia/commons/d/de/HTML5_oval_logo.png', 0, 1);
+
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Introdução ao HTML', 1, 'https://upload.wikimedia.org/wikipedia/commons/d/de/HTML5_oval_logo.png', 1, null);
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Marcadores mais usados do HTML', 2, 'https://upload.wikimedia.org/wikipedia/commons/d/de/HTML5_oval_logo.png', 1, 1);
+INSERT INTO tb_section (title, description, position, img_uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Projeto prático', 3, 'https://upload.wikimedia.org/wikipedia/commons/d/de/HTML5_oval_logo.png', 1, 2);
+
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_moment, refund_moment, available, only_update) VALUES (1, 1, TIMESTAMP '2020-11-20T13:00:00Z', null, 1, 0);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_moment, refund_moment, available, only_update) VALUES (2, 1, TIMESTAMP '2020-11-20T13:00:00Z', null, 1, 0);
